c8b2a2704c521f879f9012d662082390144ce960
dev3haven <112803130+dev3haven@users.noreply.github.com>
2026-09-18 15:42:56 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$(readlink -f "$0")")" || exit 1
-python3 mozvpn_no_externals.py --local-proxy --no-save
+
+python3 -u mozvpn_no_externals.py --local-proxy --no-save
+
 echo
 read -n 1 -s -r -p "=== Готово. Нажмите любую клавишу для выхода. ==="
 echo
