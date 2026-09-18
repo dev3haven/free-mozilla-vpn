8a96182d3d778707f76acf9007b72cd42c6c4fb2
dev3haven <112803130+dev3haven@users.noreply.github.com>
2026-09-18 15:39:22 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-if %errorlevel% neq 0 (
-    py mozvpn_no_externals.py --local-proxy --no-save
-)
+py mozvpn_no_externals.py --local-proxy --no-save
+echo.
+echo === Готово. Нажмите любую клавишу для выхода. ===
+pause >nul
