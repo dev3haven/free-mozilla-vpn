fda060991da6ee64ebf48d75388136d01b8d9751
dev3haven <112803130+dev3haven@users.noreply.github.com>
2026-09-23 15:00:55 +0300

    Create start_beta.sh

---
+++ start_beta.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "$(dirname "$(readlink -f "$0")")" || exit 1
+
+py -u mozvpn_beta.py --local-proxy --no-save
+
+echo
+read -n 1 -s -r -p "=== Готово. Нажмите любую клавишу для выхода. ==="
+echo
